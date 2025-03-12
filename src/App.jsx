0e356579b0f67385abc90aeb5f2fc323d61a5af1
ankitm99 <73329798+ankitm99@users.noreply.github.com>
2025-03-12 19:29:37 +0530

    changed to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import MainContent from "./components/MainContent";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, HashRouter  as Router, Routes } from "react-router-dom";
 import Services from "./components/Services";
 import ServiceDetails from "./components/ServiceDetails";
 import AboutUs from "./components/AboutUs";
